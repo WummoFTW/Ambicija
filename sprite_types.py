ddd2f1ab4eedaeef9c0d5789e5fb74bde744b5e0
BuildTools <unconfigured@null.spigotmc.org>
2021-09-28 14:37:49 +0300

    Collision implamentation start Sprite Classes ar causing havoc ATM

--- sprite_types.py
+++ sprite_types.py
@@ -8,3 +8,9 @@ class Main_Player(pygame.sprite.Sprite):  #Cia piesiu visus spritus
         self.image = pygame.surface([width, height]) #gali buti ir [] sklaiustai
         self.image.fill(color)
         self.rect = self.image.get_rect()
+
+#https://www.youtube.com/watch?v=hDu8mcAlY4E&ab_channel=ClearCode
+
+#https://www.youtube.com/watch?v=jO6qQDNa2UY&t=2387s&ab_channel=TechWithTim
+
+pygame.surface()
