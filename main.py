eac98cbf9eb6d04b0927b4461b52eb0c98817568
BuildTools <unconfigured@null.spigotmc.org>
2021-10-01 00:30:13 +0300

    Collision and sprite group implementation and code cleanup Currently there is still a bug with collisions need to make rotation not an in but a list

--- main.py
+++ main.py
@@ -2,39 +2,10 @@ import os
 import pygame
 import sys
 import sprite_types
+import World
 
 pygame.font.init()
 
-class World:
-    X = 0
-    Y = 0
-
-BUILDINGS = pygame.sprite.Group()
-
-class Player:
-    def __init__(self, PWIDTH, PHEIGHT):
-        self.width = PWIDTH
-        self.height = PHEIGHT
-
-    def main(self, display):
-        pygame.draw.rect(display, (0, 255, 0), (HEIGHT/2-Player.Height/4, WIDTH/2-Player.Width/2, self.width, self.height))
-
-    def collision(self):
-        if pygame.sprite.spritecollide(True, BUILDINGS):
-            if Player.Rotation == 1:
-                World.Y += Player.Speed
-            if Player.Rotation == 2:
-                World.X += Player.Speed
-            if Player.Rotation == 3:
-                World.Y += -Player.Speed
-            if Player.Rotation == 4:
-                World.X += -Player.Speed
-
-    Rotation = 1
-    Width = 55
-    Height = 103
-
-    Speed = 2.5
 
 WIDTH, HEIGHT = 1080, 1920  #Standartiniai apsirasymai, net neklausk, self explanatory
 WIN = pygame.display.set_mode((HEIGHT, WIDTH))
@@ -45,27 +16,38 @@ pygame.display.set_icon(pygame.image.load(os.path.join("Assets", "icon.png")))
 
 FONT = pygame.font.Font(os.path.join("Assets", "kongtext.ttf"), 32)
 
+WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GREEN = (0, 255, 100)
 
+Player = sprite_types.Main_Character(GREEN, 20, 30)
+Player.rect.x = HEIGHT / 2 - Player.Height / 4
+Player.rect.y = WIDTH / 2 - Player.Width / 2
+
+sprite_types.PLAYER_GROUP.add(Player)
+
+Pastatas = sprite_types.Building()
+
+sprite_types.BUILDINGS_GROUP.add(Pastatas)
 
 APPLE = pygame.transform.scale(pygame.image.load(os.path.join("Assets", "Apple.png")), (44, 52)).convert_alpha()
-DOOD = pygame.transform.scale(pygame.image.load(os.path.join("Assets", "dood_right2.png")), (Player.Width, Player.Height)).convert_alpha()
 HOUSE = pygame.transform.scale(pygame.image.load(os.path.join("Assets", "House_object.png")), (163*3, 228*3)).convert_alpha()
 TEST_GRASS2 = pygame.transform.scale(pygame.image.load(os.path.join("Assets", "map 1.1.png")), (4244*3, 3000*3)).convert()
+
+
 
 def draw_window(): #Visi piesiami dalykai eina cia <3
     WIN.blit(TEST_GRASS2, (0 + World.X, 0 + World.Y))
     WIN.blit(APPLE, (100+World.X, 600+World.Y))
-    HOUSE1_rect = pygame.Rect(200 + World.X, 0 + World.Y, 163 * 3, 228 * 3)
-    pygame.draw.rect(WIN, BLACK, HOUSE1_rect)
+    #HOUSE1_rect = pygame.Rect(200 + World.X, 0 + World.Y, 163 * 3, 228 * 3)
+    #pygame.draw.rect(WIN, BLACK, HOUSE1_rect)
     """if World.Y > -50:
         WIN.blit(DOOD, (HEIGHT / 2 - Player.Height / 4, WIDTH / 2 - Player.Width / 2))
         WIN.blits([(HOUSE, (200+ World.X, 0+World.Y)), (APPLE, (800+ World.X, 800+World.Y))])
     else:
         WIN.blits([(HOUSE, (200 + World.X, 0 + World.Y)), (APPLE, (800 + World.X, 800 + World.Y))])
         WIN.blit(DOOD, (HEIGHT / 2 - Player.Height / 4, WIDTH / 2 - Player.Width / 2))"""
-    WIN.blit(FONT.render( str(int(clock.get_fps())), True, (255, 255, 255, 255), (0, 0, 0, 255)), (1850, 6))
+    WIN.blit(FONT.render(str(int(clock.get_fps())), True, (255, 255, 255, 255), (0, 0, 0, 255)), (1850, 6))
 
 
 def controls():
@@ -76,7 +58,6 @@ def controls():
     if keypress[pygame.K_s]:    #Apacia
         World.Y += -Player.Speed
         Player.Rotation = 1
-        print(World.Y)
     if keypress[pygame.K_a]:    #Kaire
         World.X += Player.Speed
         Player.Rotation = 2
@@ -84,11 +65,16 @@ def controls():
         World.X += -Player.Speed
         Player.Rotation = 4
 
-
-main_player = sprite_types.Main_Player(Player.Width, Player.Height, HEIGHT / 2 - Player.Height, WIDTH / 2 - Player.Width, BLACK)
-
-player = Player(Player.Width, Player.Height)
-
+def collision():
+    if pygame.sprite.spritecollide(Player, sprite_types.BUILDINGS_GROUP, dokill=False):
+        if Player.Rotation == 1:
+            World.Y += Player.Speed
+        if Player.Rotation == 2:
+            World.X += -Player.Speed
+        if Player.Rotation == 3:
+            World.Y += -Player.Speed
+        if Player.Rotation == 4:
+            World.X += Player.Speed
 
 def main():  # Main loop'as check'ina visus eventus programoje for example QUIT
 
@@ -100,16 +86,19 @@ def main():  # Main loop'as check'ina visus eventus programoje for example QUIT
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-
         controls()
-        player.main(WIN)
         draw_window()
         fps_val = str(clock.get_fps())
-
+        collision()
+        Pastatas.Update(100, 100)
+        sprite_types.BUILDINGS_GROUP.update()
+        sprite_types.BUILDINGS_GROUP.draw(WIN)
+        sprite_types.PLAYER_GROUP.update()
+        sprite_types.PLAYER_GROUP.draw(WIN)
 
         pygame.display.update()
         pygame.display.flip()
-
+        print(Player.Rotation)
     sys.exit()
     pygame.quit()
 
--- sprite_types.py
+++ sprite_types.py
@@ -1,16 +1,69 @@
 import os
 import pygame
 import sys
+import World
 
-class Main_Player(pygame.sprite.Sprite):  #Cia piesiu visus spritus
-    def __init__(self, width, height, pos_x, pos_y, color):
-        #super().__init__()
-        self.image = pygame.surface([width, height]) #gali buti ir [] sklaiustai
-        self.image.fill(color)
+WHITE = (255, 255, 255)
+
+BUILDINGS_GROUP = pygame.sprite.Group()
+PLAYER_GROUP = pygame.sprite.Group()
+NPC_GROUP = pygame.sprite.Group()
+
+class Main_Character(pygame.sprite.Sprite):
+
+
+    def __init__(self, color, width, height):
+        # Call the parent class (Sprite) constructor
+        super().__init__()
+
+        # Pass in the color of the car, and its x and y position, width and height.
+        # Set the background color and set it to be transparent
+        self.image = pygame.Surface([Main_Character.Width, Main_Character.Height])
+        self.image.fill(WHITE)
+        self.image.set_colorkey(WHITE)
+
+        # Draw the car (a rectangle!)
+        #pygame.draw.rect(self.image, color, [0, 0, Main_Character.Width, Main_Character.Height])
+
+        # Instead we could load a proper picture of a car...
+        self.image = pygame.transform.scale(pygame.image.load(os.path.join("Assets", "dood_right2.png")), (Main_Character.Width, Main_Character.Height)).convert_alpha()
+
+        # Fetch the rectangle object that has the dimensions of the image.
         self.rect = self.image.get_rect()
+
+    Rotation = 1
+    Width = 55
+    Height = 103
+
+    Speed = 2.5
+
+
+class Building(pygame.sprite.Sprite):
+    # This class represents a car. It derives from the "Sprite" class in Pygame.
+
+    def __init__(self):
+        # Call the parent class (Sprite) constructor
+        super().__init__()
+
+        # Pass in the color of the car, and its x and y position, width and height.
+        # Set the background color and set it to be transparent
+        self.image = pygame.Surface([163*3, 228*3])
+        self.image.fill(WHITE)
+        self.image.set_colorkey(WHITE)
+
+        # Draw the car (a rectangle!)
+        #pygame.draw.rect(self.image, color, [0, 0, width, height])
+
+        # Instead we could load a proper picture of a car...
+        self.image = pygame.transform.scale(pygame.image.load(os.path.join("Assets", "House_object.png")), (163*3, 228*3)).convert_alpha()
+
+        # Fetch the rectangle object that has the dimensions of the image.
+        self.rect = self.image.get_rect()
+
+    def Update(self, coord_x, coord_y):
+        self.rect.center = (coord_x + World.X, coord_y + World.Y)
 
 #https://www.youtube.com/watch?v=hDu8mcAlY4E&ab_channel=ClearCode
 
 #https://www.youtube.com/watch?v=jO6qQDNa2UY&t=2387s&ab_channel=TechWithTim
 
-pygame.surface()
